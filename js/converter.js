b2431d862ed165eef014b2f2417c5cd32c3e9186
samo-za <131673420+Samo-za@users.noreply.github.com>
2024-02-06 18:33:40 +0100

    converter's update

--- js/converter.js
+++ js/converter.js
@@ -1,5 +1,21 @@
-function toKuna (euro) {
-  return euro * 7.53450;  
+function convert(event){
+  const someUserInput = document.getElementById("price")
+  if (!someUserInput) {
+    alert("Input some price first 😀")
+    return null
+  }
+  const euroValue = +someUserInput.value
+  const hrkValue = toHRK(euroValue)
+  displayResult(hrkValue)
 }
 
-console.log("The price of product is " + toKuna(1) + " kn")
+function toHRK(euro){
+  return euro * 7.53450;
+}
+toHRK()
+
+function displayResult(someValue){
+  const displayResultScreen = document.getElementById("result")
+  displayResultScreen.innerHTML = "The price of product is: " + someValue.toFixed(2) + " kn"
+}
+displayResult(0)
